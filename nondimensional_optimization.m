557e49f46806404b1ff8f078fadec9aa89e552dd
Nishant <nvparikh92@gmail.com>
2016-12-12 00:23:38 -0800

    Basic multi-path trajectory with MATLAB symbolic toolbox

--- nondimensional_optimization.m
+++ nondimensional_optimization.m
@@ -1,66 +1,127 @@
-function Parameter = nondimensional_optimization(objectiveFun, Parameter_0, tmin, tmax, pos_tmin, pos_tmax, velocity_tmin, velocity_tmax, lb, ub, maxCorridor)
-   A_EQ = generate_A_EQ(tmin, tmax);
-   B_eq = [pos_tmin; pos_tmax; velocity_tmax];
-   
-   [A_INEQ, B_ineq] = generatePositionCorridorConstraints(tmax, tmin, pos_tmax, pos_tmin, maxCorridor);
-   integral_objectiveFun = @(tParameter) numericIntegration(@(t) objectiveFun(t, tParameter), tmin, tmax);
-   Parameter = fmincon(integral_objectiveFun, Parameter_0, A_INEQ, B_ineq, A_EQ, B_eq, lb, ub);
-   t = tmin:0.05:tmax;
-   position = fliplr(Parameter) * [t.^6; t.^5; t.^4; t.^3; t.^2; t; ones(1, size(t,2))];
-   figure(1);
-   plot(t, position);
-   title('position');
-   figure(2);
-   velocity = fliplr(Parameter) * [6*t.^5; 5*t.^4; 4*t.^3; 3*t.^2; 2*t; ones(1, size(t,2)); zeros(1,size(t,2))];
-   plot(t, velocity);
-   title('velocity');
-   figure(3);
-   acceleration = fliplr(Parameter) * [5*6*t.^4; 4*5*t.^3; 3*4*t.^2; 2*3*t; 2*ones(1, size(t,2)); zeros(1,size(t,2)); zeros(1,size(t,2))];
-   plot(t,acceleration);
-   title('acceleration');
-   figure(4)
-   snap_square = objectiveFun(t, Parameter);
-   plot(t, snap_square);
-   title('snap_square');
+%About this code
+%I have accomplished generating multi path min snap trajectory
+%Using MATLAB's symbolic calculations allowing only positional constraints
+%Advantage of using symbolic toolbox is that it greatly reduces size of
+%code however it is very very slow. So should only be used when we are
+%doing symboic calculation over and over in many iterations.
+
+%Input:
+%numSegment = no. of piece wise segments
+%timeForEachSegment = absolute time allowed to finish each segment
+%maxPositionPolynomial = what polynomial size to fit
+%waypointConstraints = any positional constraints along the way
+%             format = col array of [time, position],
+%              0 >= time <= numSegment*timeForEachSegment
+%Sample input
+%waypointConstraints = [[0 0];[100 1];[0 2]];
+%nondimensional_optimization(3, 1, 6, waypointConstraints)
+
+function Parameter = nondimensional_optimization(numSegment, timeForEachSegment, maxPositionPolynomial, waypointConstraints)
+global polynomialDegree
+polynomialDegree = maxPositionPolynomial;
+
+%Storing derivatives of polynomial for faster processing
+global derivativeCache
+derivativeCache = sym(zeros(5,7));
+for index=1:5
+    derivativeCache(index, :) = getPositionDerivatives(index-1);
+end
+
+%Make sure position, velocity, acceleration and snap remain same at segment
+%boundary
+[A_EQ_segmentContinum, B_eq_segmentContinum] = getContinuityConstraintForSegmentBoundary(numSegment, timeForEachSegment, 4);
+
+%Generate constraint matrix for all positional constraints
+%And combine it with smoothness constraint at segment boundary
+A_EQ = [getConstraintsMatrix(waypointConstraints(:,2), 0, timeForEachSegment, numSegment); A_EQ_segmentContinum];
+B_eq = [waypointConstraints(:,1); B_eq_segmentContinum];
+
+%objectiveFun can be passed, but in this code it is hard coded to
+%integration(square(snap)). Integration can be done analytically in this
+%case and used. But we are numerically integrating square(snap)
+integral_objectiveFun = @(tParameter) numericIntegration(@(t) objectiveFun(t, tParameter, numSegment, timeForEachSegment), 0, timeForEachSegment*numSegment);
+
+%Col vector for all segements [param_seg1, param_seg2...]'
+initialParameterValue = zeros(numSegment*(polynomialDegree + 1), 1);
+Parameter = fmincon(integral_objectiveFun, initialParameterValue, [], [], A_EQ, B_eq);
+
+%Display trajectory found
+disp(Parameter);
+
+t = 0:0.05:timeForEachSegment*numSegment;
+position = getConstraintsMatrix(t', 0, timeForEachSegment, numSegment) * Parameter;
+figure(1);
+plot(t, position');
+end
+
+function objective = objectiveFun(t, parameter, numSegment, timeForEachSegment)
+A_EQ = getConstraintsMatrix(t', 4, timeForEachSegment, numSegment);
+objective = (A_EQ*parameter)'.^2;
+end
+
+function [A_EQ, B_eq] = getContinuityConstraintForSegmentBoundary(timeForEachSegment, numSegment, maxSmoothDerivativeAtBoundary)
+global polynomialDegree
+totalElement = polynomialDegree + 1;
+global derivativeCache
+matlab_offset = 1;
+
+A_EQ = zeros((numSegment - 1)*maxSmoothDerivativeAtBoundary, totalElement*numSegment);
+B_eq = zeros((numSegment - 1)*maxSmoothDerivativeAtBoundary, 1);
+
+times = timeForEachSegment:timeForEachSegment:timeForEachSegment*numSegment;
+for index= 1:size(times, 2) - 1
+    time = times(index);
+    start = (index -1)*totalElement + matlab_offset;
+    for smoothDerivative =1:maxSmoothDerivativeAtBoundary
+        constraintRow = double(subs(derivativeCache(smoothDerivative, :), time));
+        
+        %The way we make sure derivative is same at segment boundary is by
+        %[polynomial_derivative, - polnomial_derivate,...] *
+        %[params1,params2,...]' = [0...]. Thats why B_eq is all zero
+        %Also we generate each row for as many smooth derivative like
+        %position, velocity, acceleration etc. we want
+        A_EQ((index - 1)*maxSmoothDerivativeAtBoundary + smoothDerivative, start:start+totalElement*2-1) = [constraintRow, -constraintRow];
+    end
+end
+end
+
+%Input:
+%times = generate constraints for each time in times
+function A_EQ_constraints = getConstraintsMatrix(times, whichDerivative, timeForEachSegment, numSegment)
+global polynomialDegree
+totalElement = polynomialDegree + 1;
+global derivativeCache
+A_EQ_constraints = zeros(size(times,1), totalElement*numSegment);
+for index = 1:size(times, 1)
+    time = times(index);
+    segmentNo = max(0, ceil(time/timeForEachSegment) - 1); %segmentNo starts from 0, handling special case of 0
+    A_EQ_constraints(index, :) =  generateRowForSegment(double(subs(derivativeCache(whichDerivative+1, :), time)), segmentNo, numSegment);
+end
+end
+
+%actualRow = row containing constraints for segments
+%segmentNo = segment for which to put actualRow in.
+function row = generateRowForSegment(actualRow, segmentNo, numSegment)
+global polynomialDegree
+totalElement = polynomialDegree + 1; %including constant
+row = zeros(1, totalElement * numSegment);
+matlab_offset = 1;
+start = matlab_offset+segmentNo*totalElement;
+row(start:start+polynomialDegree) = row(start:start + polynomialDegree) + actualRow;
 end
 
 function integration = numericIntegration(fun, tmin, tmax)
-    integration = integral(fun, tmin, tmax);
+integration = integral(fun, tmin, tmax);
 end
 
-function A_EQ = generate_A_EQ(tmin, tmax)
-    A_EQ = [1 tmin tmin^2 tmin^3 tmin^4 tmin^5 tmin^6; 
-            1 tmax tmax^2 tmax^3 tmax^4 tmax^5 tmax^6;
-            %0 1 2*tmin 3*tmin^2 4*tmin^3 5*tmin^4 6*tmin^5;
-            0 1 2*tmax 3*tmax^2 4*tmax^3 5*tmax^4 6*tmax^5];
+function dpositionWoParamsdx = getPositionDerivatives(numDerivatives)
+global polynomialDegree
+totalElement = polynomialDegree + 1; %including constant
+x = [];
+syms x;
+positionWOParams = sym('x', [1  totalElement]);
+for degree=0:polynomialDegree
+    positionWOParams(degree + 1) = power(x, degree);
 end
-
-% A_INEQ*Parameter - 
-%(A_INEQ*Parameter . UnitVector(along trajectory)) UnitVector^)
-% < maxCorridor
-function [A_INEQ, B_ineq] = generatePositionCorridorConstraints(tmax, tmin , pmax, pmin, maxCorridor)
-    no_constraints = 30;
-    unitVector = get_unitVector_straight_path(pmax, pmin, tmax, tmin);
-    tConstraints = tmin:(tmax - tmin)/(no_constraints - 1):tmax;
-    A_INEQ = zeros(no_constraints, 7);
-    for iter = 1:no_constraints
-        t = tConstraints(iter);
-        A_INEQ(iter,:) = [1 t t^2 t^3 t^4 t^5 t^6];
-    end
-    A_INEQ = A_INEQ * (1 - unitVector(1)^2);
-    %replicate each row for > and <, since comparison is for absolute
-    %values
-    A_INEQ = kron(A_INEQ, [1; -1]);
-    
-    B_ineq = (maxCorridor*ones(no_constraints, 1));
-    B_ineq = kron(B_ineq, [1; 1]) + kron((tConstraints' - tmin)*unitVector(1)*unitVector(2), [1; -1]);% |pos| <= max_corridor
-end
-
-function unitVector = get_unitVector_straight_path(pmax, pmin, tmax, tmin)
-    unitVector_straight_path_magnitude = sqrt((pmax - pmin)^2 + (tmax - tmin)^2);
-    unitVector = [(pmax - pmin) / unitVector_straight_path_magnitude (tmax - tmin)/unitVector_straight_path_magnitude];
-end
-
-function val = test(t)
-  val = [20*360 1*240 34*120 0 0 0 0] * [t.^2 t.^ t.^4 t.^3 t.^2 t 1]';
+dpositionWoParamsdx = diff(positionWOParams, numDerivatives);
 end
